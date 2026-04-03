e45bbf318ee81d09756dc183b13fd6cbfdc8146a
suboczsubocz702 <suboczmateusz08@gmail.com>
2026-04-03 15:25:26 +0200

    Create C1.sol

---
+++ blockchainL2/C1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31;
+
+contract NumberList {
+    uint[] public numbers;
+    function addNumber(uint _number) public {
+        numbers.push(_number);
+    }
+    function getNumber(uint index) public view returns (uint) {
+        require(index < numbers.length, "Niepoprawny indeks");
+        return numbers[index];
+    }
+    function getLength() public view returns (uint) {
+        return numbers.length;
+    }
+}
