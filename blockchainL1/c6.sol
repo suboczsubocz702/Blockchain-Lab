44d91538ac04e47abbb6889aa4b3f42eb3deacfc
suboczsubocz702 <suboczmateusz08@gmail.com>
2026-04-03 15:15:12 +0200

    Create c6.sol

---
+++ blockchainL1/c6.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CompareNumbers {
+
+    function compare(uint a, uint b) public pure returns (string memory) {
+        if (a == b) {
+            return "Rowne";
+        } else if (a > b) {
+            return "A wieksze";
+        } else {
+            return "B wieksze";
+        }
+    }
+}
