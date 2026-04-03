042ef2306443bbedb7bba7501ffe4031c1610cf9
suboczsubocz702 <suboczmateusz08@gmail.com>
2026-04-03 15:15:41 +0200

    Create C7.sol

---
+++ blockchainL1/C7.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MinNumber {
+
+    function min(uint a, uint b) public pure returns (uint) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
